b260809a73c85bb45beab7c93859c7583e611b7b
sietefx <sietefx@gmail.com>
2025-10-08 10:03:36 -0300

    Primeiro desafio Dio iOS

--- primeiroDesafioXCode.playground/Contents.swift
+++ primeiroDesafioXCode.playground/Contents.swift
@@ -1,3 +1,14 @@
-import UIKit
+var firstName = "Steve"
+var lastName: String? = "Jobs"
 
-var greeting = "Hello, playground"
+// Print usando nil-coalescing como antes
+print("\(firstName) \(lastName ?? "Wozniak")")
+
+// Optional binding para desembrulhar e imprimir usando a variável desembrulhada
+let finalLastName: String
+if let unwrappedLastName = lastName {
+    finalLastName = unwrappedLastName
+} else {
+    finalLastName = "Wozniak"
+}
+print("\(firstName) \(finalLastName)")
